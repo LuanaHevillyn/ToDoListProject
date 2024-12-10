00291bed441deecbba927f414e66b9a0070c25d6
Luana Hevillyn <luanahevillynn@gmail.com>
2024-12-09 21:45:33 -0300

    Initial commit: To-Do List with Quasar Framework

--- quasar.config.js
+++ quasar.config.js
@@ -90,6 +90,7 @@ module.exports = configure(function (/* ctx */) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
+      lang: 'pt',
       config: {},
 
       // iconSet: 'material-icons', // Quasar icon set
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,47 +1,165 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page>
+    <div class="q-pa-md row flex-center absolute-center">
+      <q-card bordered class="my-card shadow-4" :grid="$q.screen.xs">
+        <q-card-section>
+          <div class="text-h5 text-center text-accent">To-do List</div>
+        </q-card-section>
+        <q-card-section>
+          <div class="row">
+            <div class="col">
+              <q-input
+                clearable
+                clear-icon="close"
+                outlined
+                color="purple"
+                v-model="taskInput"
+                label="Ex: Praticar natação"
+              />
+            </div>
+            <div class="col-grow">
+              <q-btn
+                unelevated
+                padding="md"
+                color="accent"
+                label="Adicionar"
+                @click="createTask(taskInput)"
+              />
+            </div>
+          </div>
+        </q-card-section>
+
+        <q-separator inset />
+        <q-card-section>
+          <div class="text-center">
+            <q-checkbox
+              left-label
+              label="Filtrar concluídos: "
+              name="seeTasksCheckbox"
+              v-model="seeCompletedTasks"
+              color="accent"
+            />
+          </div>
+          <q-table
+            flat
+            bordered
+            :rows="filteredTasks"
+            :columns="columns"
+            row-key="name"
+            selection="single"
+            v-model:selected="selected"
+            :dense="$q.screen.lt.md"
+          >
+            <template v-slot:body="props">
+              <q-tr :props="props">
+                <q-td>
+                  <q-checkbox
+                    :name="'checkbox' + props.row"
+                    v-model="props.row.status"
+                    :true-value="Status.Concluído"
+                    :false-value="Status.Pendente"
+                    color="accent"
+                  />
+                </q-td>
+                <q-td key="selected" :props="props">
+                  {{ props.row.name }}
+                </q-td>
+                <q-td key="name" :props="props">
+                  {{ props.row.name }}
+                </q-td>
+                <q-td key="status" :props="props">
+                  <q-badge :color="changeStatusColor(props.row.status)">
+                    {{ props.row.status }}
+                  </q-badge>
+                </q-td>
+                <q-td key="actions" :props="props">
+                  <q-btn unelevated @click="deleteTask(props.row)">
+                    <q-icon size="2em" name="delete" color="red" />
+                  </q-btn>
+                </q-td>
+              </q-tr>
+            </template>
+            <template v-slot:no-data="{ message }">
+              <div class="full-width row flex-center text-accent q-gutter-sm">
+                <q-icon size="2em" name="sentiment_dissatisfied" />
+                <span>{{ message }}</span>
+              </div>
+            </template>
+          </q-table>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import { QTableColumn } from 'quasar';
+import { computed, ref } from 'vue';
 
-defineOptions({
-  name: 'IndexPage'
-});
+const taskInput = ref('');
+const selected = ref([]);
+const tasks = ref<TaskObject[]>([]);
+const seeCompletedTasks = ref(false);
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
+enum Status {
+  Pendente = 'Pendente',
+  Concluído = 'Concluído',
+}
+
+interface TaskObject {
+  name: string;
+  status: Status;
+}
+
+function createTask(taskName: string): void {
+  if (taskName.trim() !== '') {
+    tasks.value.push({
+      name: taskName,
+      status: Status.Pendente,
+    });
+    taskInput.value = '';
   }
-]);
+}
 
-const meta = ref<Meta>({
-  totalCount: 1200
+function deleteTask(index: number): void {
+  tasks.value.splice(index, 1);
+}
+
+function changeStatusColor(taskStatus: Status): string {
+  if (taskStatus === Status.Concluído) {
+    return 'green';
+  }
+  return 'orange';
+}
+
+const columns: QTableColumn[] = [
+  {
+    name: 'name',
+    required: true,
+    label: 'Tarefa',
+    align: 'left',
+    field: 'name',
+    sortable: true,
+  },
+  {
+    name: 'status',
+    align: 'center',
+    label: 'Status',
+    field: 'status',
+    sortable: true,
+  },
+  {
+    name: 'actions',
+    align: 'center',
+    label: '',
+    field: 'actions',
+    sortable: true,
+  },
+];
+
+const filteredTasks = computed(() => {
+  return seeCompletedTasks.value
+    ? tasks.value.filter((task) => task.status === Status.Concluído)
+    : tasks.value;
 });
 </script>
